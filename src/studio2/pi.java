fc2105ed4635553ec149bb10dffb2451f7212b00
jfarah38 <109042417+jfarah38@users.noreply.github.com>
2025-09-15 11:24:36 -0500

    finished

--- src/studio2/pi.java
+++ src/studio2/pi.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 public class pi {
     public static void main(String[] args) {
-        double x = Math.random();
-        double y = Math.random();
+        Scanner in = new Scanner(System.in);
+        System.out.println("How many darts would you like to throw?");
+        int numOfDarts = in.nextInt();
+        int insideCounter = 0;
+        for (int d = 0; d<numOfDarts; d++) {
+            double x = Math.random();
+            double y = Math.random();
+            double distance = Math.sqrt((Math.pow((x-0.5),2))+Math.pow((y-0.5),2));
+            if (distance < 0.5) {
+                insideCounter++;
+            }
+            
+        }
+        System.out.println(((double)insideCounter / numOfDarts) * 4);
         
 
     }
